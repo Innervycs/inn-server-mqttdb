1d070043d4b612b4ef157d38a7c2ed7e47d5c7a4
efrain.ra7 <efrain.reyes@innervycs.com>
2025-07-17 16:50:43 -0400

    fix en init_db cambio de codigo para creacion de db

--- db/init_db.sql
+++ db/init_db.sql
@@ -1,7 +1,1 @@
-# Crear base de datos si no existe
-if ! sudo -u postgres psql -tAc "SELECT 1 FROM pg_database WHERE datname = 'iot'" | grep -q 1; then
-  echo "Creando base de datos iot..."
-  sudo -u postgres createdb -O iot iot
-else
-  echo "La base de datos iot ya existe"
-fi
+CREATE DATABASE iot OWNER iot;
